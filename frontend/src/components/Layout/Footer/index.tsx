35cbec6905453ad1e564e5a414970d574f544dab
Mohammad Shahin <59062739+MohammadShahin@users.noreply.github.com>
2022-12-14 16:35:52 +0300

    add layout footer

---
+++ frontend/src/components/Layout/Footer/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Link from 'next/link'
+import styles from './Footer.module.css'
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <p className={styles.footer}>
+                This project was done as part of the Blockchain: Distributed
+                Ledger Development course in{' '}
+                <Link
+                    className={styles.a}
+                    href="https://innopolis.university/en/"
+                >
+                    Innopolis University
+                </Link>
+            </p>
+        </footer>
+    )
+}
